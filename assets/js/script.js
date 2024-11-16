1aee6b6fe7a4f83c15f659444e3517519a9b55dc
taiki-yokoyama <taikig0806yoko@gmail.com>
2024-11-17 02:51:47 +0900

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@ document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.splide' );
     splide.mount();
 } );
+const header = document.getElementById("header");
+document.addEventListener("scroll", () => {
+    var y = window.scrollY;
+    if(y>500){
+        
+        header.classList.remove("is-transpalent");
+    }else{
+        header.classList.add("is-transpalent");
+    }
+    console.log("hhelo");
+});
